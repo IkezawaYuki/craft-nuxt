97998a09ee62795d343cdcf73535695cafe0cb2d
yikezawa <y.ikezawa93@gmail.com>
2020-07-16 23:15:19 +0900

    mod

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,8 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { href: 'https://fonts.googleapis.com/icon?family=Material+Icons', rel: 'stylesheet' }
     ]
   },
   /*
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,37 @@
 <template>
-  
+  <div class="container">
+    <chart
+      :chartType="chartType"
+      :chartData="chartData"
+      :chartOptions="chartOptions"
+    />
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import Chart from '@/components/Chart.vue'
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
+    Chart
+  },
+  data() {
+    return {
+      chartType: 'PieChart',
+      chartData: [
+        ['年', '売上', '費用', '収益'],
+        ['2014', 1000, 400, 200],
+        ['2015', 1170, 460, 250],
+        ['2016', 660, 1120, 300],
+        ['2017', 1030, 540, 350]
+      ],
+      chartOptions: {
+        title: '会社の損益',
+        subtitle: '売上',
+        width: 500,
+        height: 500
+      }
+    }
   }
 }
 </script>
